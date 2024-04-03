f376ed6fc0acd5f6d6290ce9b0cbd30199750103
Programmer-M1shka <xaritona12@gmail.com>
2024-04-03 14:37:39 +0400

    main

---
+++ main.js
@@ -0,0 +1,30 @@
+const inputbox = document.getElementById('input-box');
+const listcon = document.getElementById('listcon');
+
+
+function add(){
+    if (inputbox.value === ''){
+        alert('WRONG COMMAND')
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputbox.value;
+        listcon.appendChild(li);
+        let span = document.createElement("span")
+        span.innerHTML = "<i class='bx bx-message-square-x' ></i>";
+        li.appendChild(span);
+    }
+    inputbox.value = "";
+    saveData();
+}
+
+listcon.addEventListener("click", function(e) {
+    if (e.target.tagName === "LI") { 
+        e.target.classList.toggle("correct");
+        saveData();
+    } 
+    else if (e.target.tagName === "I") {
+        e.target.parentElement.parentElement.remove();
+        saveData();
+    }
+});
